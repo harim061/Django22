0af8b5789c7fd32a12ccef78c084b801cf453ba5
harim061 <doris5093@duksung.ac.kr>
2022-10-18 16:51:16 +0900

    포스트 상세 페이지 테스트

--- blog/tests.py
+++ blog/tests.py
@@ -46,5 +46,12 @@ class TestView(TestCase):
         soup = BeautifulSoup(response.content, 'html.parser')
 
         navbar = soup.nav
-        self.assertIn('Blogs', navbar.text)
+        self.assertIn('Blog', navbar.text)
         self.assertIn('AboutMe', navbar.text)
+
+        self.assertIn(post_001.title, soup.title.text)
+
+        main_area =soup.find('div',id='main-area')
+        post_area = main_area.find('div',id='post-area')
+        self.assertIn(post_001.title,post_area.text)
+        self.assertIn(post_001.content,post_area.text)
