9dc045096e33a3c0ec0ad985bd48ba7bbb049e68
harim061 <doris5093@duksung.ac.kr>
2022-11-03 14:44:20 +0900

    다대다 관계 구현하기 완료

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from .models import Post,Category
+from .models import Post,Category,Tag
 from django.views.generic import ListView,DetailView
 # Create your views here.
+
 
 class PostList(ListView):
     model = Post
@@ -42,6 +43,20 @@ def category_page(request,slug):
     )
     # 템플릿 모델명_detail.html : post_detail.html
     # 파라미터 모델명 : post
+
+def tag_page(request,slug):
+    tag =Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all()
+
+    return render(
+        request, 'blog/post_list.html',{
+            'tag': tag,
+            'post_list': post_list,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count
+        }
+
+    )
 
 #def index(request):
 #    posts1 = Post.objects.all().order_by('-pk')
